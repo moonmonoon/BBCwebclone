87073481ff092aea1a260c384e69da87b5b065c7
moonmonoon <moonmonoon@gmail.com>
2021-09-04 23:14:46 +0900

    Fix for-loop : use IntersectionObserver

--- main.js
+++ main.js
@@ -4,8 +4,15 @@
   const graphicElems = document.querySelectorAll('.graphic-item');
   // 현재 활성화된 (visible 클래스가 붙은) .graphic-item을 지정
   let currentItem = graphicElems[0]; 
+  let ioIndex;
+
+  const io = new IntersectionObserver((entries, observer) => {
+    // console.log(entries[0].target.dataset.index * 1);
+    ioIndex = entries[0].target.dataset.index * 1;
+  });
 
   for (let i = 0; i < stepElems.length; i++) {
+    io.observe(stepElems[i]);
     // stepElems[i].setAttribute('data-index', i);
     stepElems[i].dataset.index = i;
     graphicElems[i].dataset.index = i;
@@ -22,11 +29,17 @@
   window.addEventListener('scroll', () => {
     let step;
     let boundingRect;
+    // let temp = 0; // temp = 루프가 총 몇번 도는지 확인하는 용도
 
-    for (let i =0; i < stepElems.length; i++) {
+    //for (let i = 0; i < stepElems.length; i++) {
+    for (let i = ioIndex - 1; i < ioIndex + 2; i++) {
       step = stepElems[i];
+      if (!step) continue; // ioIndex - 1 조건 오류 해결
       boundingRect = step.getBoundingClientRect();
       //console.log(boundingRect.top);
+
+      // temp++;
+
       if (boundingRect.top > window.innerHeight * 0.1 && 
         boundingRect.top < window.innerHeight * 0.8) {
         //console.log(step.dataset.index);
@@ -35,6 +48,7 @@
         activate();
       }
     }
+    // console.log(temp);
   });
 
   activate();
